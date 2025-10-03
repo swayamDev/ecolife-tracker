7c70e0f939e3b0fa37eb57e2411b0c1082489d5f
Swayam Swarup Panda <swayamcoder@gmail.com>
2025-10-03 15:49:08 +0530

    Docs: update deployment steps

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:
-    mode === "production" && process.env.DEPLOY_TARGET === "gh-pages"
-      ? "/ecolife-tracker/"
-      : "/",
+  base: process.env.DEPLOY_TARGET === "gh-pages" ? "/ecolife-tracker/" : "/",
   server: {
     host: "::",
     port: 8080,
